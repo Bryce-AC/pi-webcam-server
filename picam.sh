ac4ec2cb4a6301174a2ec64fe006d112e95b7404
BryceAC <39605594+Bryce-AC@users.noreply.github.com>
2020-04-07 22:26:53 +0930

    removed preview from desktop

--- picam.sh
+++ picam.sh
@@ -1,1 +1,1 @@
-raspivid -o - -t 0 -hf -w 1920 -h 1080 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8099}' :demux=h264 :h264-fps=30
+raspivid -o - -n -t 0 -hf -w 1920 -h 1080 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8099}' :demux=h264 :h264-fps=30
