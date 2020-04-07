4067a9b8430d710ccce2b005e95d56aba76c6e8a
BryceAC <39605594+Bryce-AC@users.noreply.github.com>
2020-04-07 22:22:51 +0930

    accidentally fixed picam
    added `:h264-fps=30` to end, tells how many frames for camera, stops buffer problems(?)

--- picam.sh
+++ picam.sh
@@ -1,2 +1,1 @@
-raspivid -o - -t 0 -hf -w 1920 -h 1080 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8099}' :demux=h264
-
+raspivid -o - -t 0 -hf -w 1920 -h 1080 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8099}' :demux=h264 :h264-fps=30
